249140dc7bfc8988e26b572059d7eb0b469fa786
Ian Flynn <ianflynnwork@gmail.com>
2024-05-21 14:48:23 -0400

    Add image, and change tsconfig to have base route

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,10 +1,19 @@
+import headshot from '@/public/Ian-Flynn-Headshot.jpg';
+import Image from 'next/image';
+
 const Intro = () => {
   return (
-    <div>
+    <section>
+      <Image
+        src={headshot}
+        alt='Ian enjoying a hike'
+        className='h-60 w-60 object-cover aspect-square w-5/6 rounded-full border border-black '
+      />
       <h1>Ian Flynn</h1>
+
       <p className=''>Fullstack Software Engineer</p>
       <p>Crafting useful, maintainable, and captivating web products.</p>
-    </div>
+    </section>
   );
 };
 
