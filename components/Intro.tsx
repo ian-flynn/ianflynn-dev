b2b2498281855f3ea18b6f17fe536b56adb5ed61
Ian Flynn <ianflynnwork@gmail.com>
2024-05-22 16:18:03 -0400

    simplified all classnames

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,17 +3,19 @@ import Image from 'next/image';
 
 const Intro = () => {
   return (
-    <section className='text-2xl bg-red-200 h-screen flex flex-col justify-center items-center'>
+    <section className='text-2xl h-screen flex flex-col justify-center items-center text-center'>
       <Image
         src={headshot}
         alt='Ian enjoying a hike'
-        className='w-60 object-cover aspect-square rounded-full relative ml-auto'
+        className='w-60 object-cover aspect-square rounded-full mb-3'
       />
-      <p className='mr-auto text-2xl'>Hi! My name is</p>
-      <h1 className='mr-auto text-2xl font-bold'> Ian Flynn.</h1>
+      <p className='text-xl'>
+        <span className='font-medium'>Hi!</span> My name is
+      </p>
+      <h1 className=' text-4xl font-bold'> Ian Flynn</h1>
       <p>
         I build full-stack web apps in Typscript, React, Next.js and Node.
-        Currently living in South Florida!
+        Currently living in Juno Beach, Florida!
       </p>
     </section>
   );
